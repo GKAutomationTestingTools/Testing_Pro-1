eb47b56fdd808de4f652ca10c0471705b9313000
Gk <66637255+GKAutomationTestingTools@users.noreply.github.com>
2020-09-12 23:09:24 +0530

    Update Test2.java

--- src/test/java/com/GitRepository/Git_Repoitory_Branches/Test2.java
+++ src/test/java/com/GitRepository/Git_Repoitory_Branches/Test2.java
@@ -3,8 +3,7 @@ package com.GitRepository.Git_Repoitory_Branches;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+System.out.println("Hello ");
 	}
 
 }
