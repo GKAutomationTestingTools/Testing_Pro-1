e6efe70dee58a05ce412aba59e3c2705244489f7
Admin <Gk_Autor>
2020-09-12 22:57:14 +0530

    Testing Git Project

---
+++ src/test/java/com/GitRepository/Git_Repoitory_Branches/Test1.java
@@ -0,0 +1,19 @@
+package com.GitRepository.Git_Repoitory_Branches;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		System.out.println(" Sum of value is => "+(a+b));
+		
+		
+		
+		
+		
+		
+	}
+
+}
