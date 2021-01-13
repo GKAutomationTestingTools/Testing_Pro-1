ddc3b50f8ca1b388c6fac72e73de2a795e24abe1
Admin <Admin@DESKTOP-1K0N594>
2021-01-13 15:12:19 +0530

    Second Commit

---
+++ src/main/java/com/GitRepository/Git_Repoitory_Branches/Employee_class.java
@@ -0,0 +1,12 @@
+package com.GitRepository.Git_Repoitory_Branches;
+
+public class Employee_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("employee class");
+	}
+
+}
--- src/test/java/com/GitRepository/Git_Repoitory_Branches/Test2.java
+++ src/test/java/com/GitRepository/Git_Repoitory_Branches/Test2.java
@@ -3,7 +3,8 @@ package com.GitRepository.Git_Repoitory_Branches;
 public class Test2 {
 
 	public static void main(String[] args) {
-System.out.println("Hello ");
+       
+		System.out.println("Hello ");
 	}
 
 }
